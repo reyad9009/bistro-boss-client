f63acc6b1ee4a612ba32963612caea0692835e3d
reyad9009 <tarekgithub09@gmail.com>
2025-01-19 17:39:19 +0600

    complete final project part 5

--- src/layout/Dashboard.jsx
+++ src/layout/Dashboard.jsx
@@ -9,14 +9,17 @@ import {
   FaShoppingCart,
   FaUsers,
   FaUtensils,
-
 } from "react-icons/fa";
 import { NavLink, Outlet } from "react-router-dom";
 import useCart from "../hooks/useCart";
+import useAdmin from "../hooks/useAdmin";
+
 
 const Dashboard = () => {
   const [cart] = useCart();
-  const isAdmin = true;
+
+  // TODO: get isAdmin value from the database
+  const [isAdmin] = useAdmin();
 
   return (
     <div className="flex">
@@ -90,8 +93,6 @@ const Dashboard = () => {
               </li>
             </>
           )}
-
-
           {/* shared nav links */}
           <div className="divider"></div>
           <li>
